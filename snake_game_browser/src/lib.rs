1498b671d69c55301e6841436260453a4f967681
Senne van Doorn <senne@squaredpaper.co.uk>
2025-03-03 22:03:07 +0000

    Start of snake game - Setup so rust and js is glued

--- snake_game_browser/src/lib.rs
+++ snake_game_browser/src/lib.rs
@@ -1,9 +1,24 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-   println!("Hi there {}", name); 
+pub struct World {
+    width: usize
 }
 
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> World {
+        World {
+            width: 8
+        }
+    }
+     
+    pub fn width(&self) -> usize {
+        self.width
+    }
+}
 
 // wasm-pack build --target web
--- snake_game_browser/www/index.js
+++ snake_game_browser/www/index.js
@@ -1,33 +1,6 @@
-async function init() {
-
-  const memory = new WebAssembly.Memory({initial: 1});
-
-  const importObject = {
-    js: {
-      mem: memory
-    },
-    console: {
-      log: () => {
-        console.log("Just logging something")
-      },
-      error: () => {
-        console.log("I am just error")
-      }
-    }
-  }
-
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
-
-  const sumFunction = wasm.instance.exports.sum;
-  const uint8Array = new Uint8Array(memory.buffer, 0, 2)
-
-  const hiText = new TextDecoder().decode(uint8Array)
-
-  const results = sumFunction(70, 80);
-  console.log(results)
-  console.log(hiText)
-}
-
-init();
+import init, { World } from "snake_game_browser"
+init().then(_ => {
+  const world = World.new()
+  const canvas = document.getElementById("snake-canvas")
+  const ctx = canvas.getContext("2d")
+})
